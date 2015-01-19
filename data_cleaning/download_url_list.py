8eb3e9375f086692d19244ddda6757624afcfe9c
Rachel Reddick <rr4idic@mac.com>
2015-01-19 15:44:57 -0800

    Finished writing main functions for setting up the mysql database

--- data_cleaning/download_url_list.py
+++ data_cleaning/download_url_list.py
@@ -3,6 +3,8 @@
 import urllib
 import json
 
+import MySQLdb as mdb
+
 import title_cleanup
 
 def read_single_dataset_from_kimono(address,apikey,koffset):
@@ -120,12 +122,147 @@ def collect_indiedb_data():
 
     return title, creator, release_date, engine, rating, votes, type, theme, players, platform, description
 
-def cleanup_and_put_in_database(title, creator, release_data, engine, rating, votes, type, theme, players, platform, description):
+def split_list(text_list):
+    '''
+    Splits a comma-separated list of items and returns them with white space removed
+    '''
+
+    values = text_list.split(',')
+    
+    for i in range(len(values)):
+        values[i] = values[i].strip()
+
+    return values
+
+def get_tidy_modes_list(players):
+    #Split the players into separate categories, and give 0 or 1 for whether that mode exists
+    play_mode = np.zeros([len(players), 4]) #Columns are single, multiplayer, coop, MMO
+    for i in range(ngames):
+        clean_play = split_list(players[i])
+        if 'Single' in clean_play or 'Single Player' in clean_play:
+            clean_play[i,0] = 1
+        if 'Multiplayer' in clean_play:
+            clean_play[i,1] = 1
+        if 'Co-op' in clean_play:
+            clean_play[i,2] = 1
+        if 'MMO' in clean_play:
+            clean_play[i,3] = 1
+
+    return clean_play
+
+def get_tidy_platform(platform):
+    #Get the list of possible platforms
+    #Get the unique listing first
+    temp_unique = np.unique(platform)
+    #Break this down by comma-separated platforms
+    uniq_p_list = []
+    for some_plats in temp_unique:
+        split_plats = split_list(some_plats)
+        for plat in split_plats:
+            uniq_p_list.append(plat)
+    #Get the final uniq results
+    plat_list = np.unique(uniq_p_list)
+    nplats = len(plat_list)
+
+    #Now, tidy up
+    clean_plats = np.zeros([len(platform),nplats])
+    for i in range(len(platform)):
+        single_platform_list = split_list(platform[i])
+        for j in range(nplats):
+            if plat_list[j] in single_platform_list:
+                clean_plats[i,j] = 1
+
+    return clean_plats, plat_list
+
+def cleanup_and_put_in_database(title, creator, release_date, engine, rating, votes, type, theme, players, platform, description,
+                                cursor):
     '''
     Performs additional cleanup and puts the rest of the data into an SQL table for later retrieval
+    Requires a mysql cursor
     '''
+
+    #Get number of games
+    ngames = len(title)
+
+    #Clean the unicode from the creator names
+    creator_old = np.copy(np.array(creator))
+    for i in range(ngames):
+        creator[i] = title_cleanup.replace_right_quote(creator_old[i])
+
+    #Split up the player modes into a nice matrix
+    clean_play = get_tidy_modes_list(players)
+
+    #Split up the platform lists as well
+    clean_plats,unique_platforms = get_tidy_platform(platform)
+
+    #Save most results into a temporary file
+    f = open("../game_basics_list.txt",'w')
+    for i in range(len(title)):
+        print >> f, title[i]+','+creator[i]+','+release_date[i]+','+engine[i]+','+rating[i]+','+votes[i]+','+type[i]+','+theme[i]+','+players[i]+','+platform[i]    
+
+    f.close()
+    
+    #Fix types
+    votes = np.array(votes).astype(int)
+    rating = np.array(rating)
+    unrated = np.where(rating == '-')[0]
+    rating[unrated] = 0*unrated -1
+    rating = np.array(rating).astype(float)
+
+    #Create the initial database
+    cursor.execute("DROP TABLE IF EXISTS Games")
+    
+    #Setup the creation table.  Note that we have a medium int value for each mode and platform
+    command_create_table = "CREATE TABLE Games(Id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR[100], creator VARCHAR[100], engine VARCHAR[100], rating FLOAT, votes INT, "
+    command_create_table = "release_day INT, release_year INT, release_month VARCHAR[4], "
+    command_create_table += "type VARCHAR[50], theme VARCHAR[50], single_player BOOL, multiplayer BOOL, coop BOOL, mmo BOOL "
+    for some_platform in plat_list:
+        command_create_table += ", "+some_platform+" BOOL"
+    command_create_table += ")"
+    cursor.execute(command_create_table)
+    values_names = "title, creator, engine, rating, votes, release_day, release_year, release_month, type, theme, single_player, multiplayer, coop, mmo"
+    for some_platform in plat_list:
+        values_names += ", "+some_platform
+    for i in range(ngames):
+        insert_statement = "INSERT INTO Games("+values_names+") VALUES ('"+title[i]+"', '"+creator[i]+"', '"+engine[i]+"', "+str(rating[i])+", "+str(votes[i])
+        insert_statement += ", "+release_date[4:6]+", "+release_date[-4:]+", "+release_date[:3]+", '"+type[i]+"', '"+theme[i]+"', "+str(clean_play[i,0])+", "
+        insert_statement += str(clean_play[i,1])+", ", +str(clean_play[i,2])+ ", " + str(clean_play[i,3])
+        for j in len(plat_list):
+            insert_statement += ", "+str(clean_plats[i,j])
+        insert_statement += "))"
+        cursor.execute(insert_statement)
+
+    print "Finished inserting main tags, moving to descriptions"
+        
+    #Save plain text descriptions in a second table
+    cursor.execute("DROP TABLE IF EXISTS Game_descr")
+    cursor.execute("CREATE TABLE Game_descr(Id INT PRIMARY KEY AUTO INCREMENT, description VARCHAR[2001])")
+    for i in range(ngames):
+        cursor.execute("INSERT INTO Game_descr(description) VALUES ('"+description[i]+"')")
+
+    print "Table insertion complete"
+
     return
 
 if __name__ == '__main__':
-    #print_urllist()
-    pass
+    #Getting mysql login data
+    f = open("../login.txt")
+    mysql_login = f.read()
+    f.close()
+    mysql_login = mysql_login.split()
+
+    hostname = mysql_login[0]
+    username = mysql_login[1]
+    password = mysql_login[1]
+
+    #Read the data in
+    title, creator, release_date, engine, rating, votes, type, theme, players, platform, description = collect_indie_db_data()
+
+    con = mdb.connect(hostname,username,password,'indiedb')
+
+    with con:
+        cursor = con.cursor()
+        cleanup_and_put_in_database(title, creator, release_date, engine, rating, votes, type, theme, players, platform, description,
+                                    cursor)
+        
+    
