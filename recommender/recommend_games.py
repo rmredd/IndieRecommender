ddaccbdd0c88c796adfcd8293aea1d9aafb84ba5
Rachel Reddick <rr4idic@gmail.com>
2015-01-23 10:04:25 -0800

    Basic data retrieval works

--- recommender/recommend_games.py
+++ recommender/recommend_games.py
@@ -28,11 +28,19 @@ def get_matching_indie_games(genre, game_type, num_players, ):
     cur.execute(columns_command)
     words_list = cur.fetchall()
 
-    select_command = "SELECT Games.title, Games.rating, Games.votes, Games.genre FROM Games JOIN Summary_words ON Games.Id = Summary_words.game_id WHERE"
-    select_command += "Games.genre = '"+genre+"' AND Games.game_type LIKE '"+game_type+"' ORDER BY votes, rating DESC"
+    select_command = "SELECT Games.title, Games.rating, Games.votes, Games.theme, Games.game_type "
+    for word in words_list[2:]:
+        select_command += ", Summary_words."+word[0]
+    select_command += " FROM Games JOIN Summary_words ON Games.Id = Summary_words.game_id WHERE "
+    select_command += num_players+"= 1 AND "
+    select_command += "Games.theme = '"+genre+"' AND Games.game_type LIKE '"+game_type+"' ORDER BY Games.votes"
+    cur.execute(select_command)
+    game_data = cur.fetchall()
 
-    return game_data, words_list
+    return game_data, words_list[2:]
 
+def make_metacritic_game_word_vector():
+    return
 
 if __name__ == '__main__':
     con = login_mysql("../login.txt")
@@ -42,4 +50,14 @@ if __name__ == '__main__':
         
         mygame = get_metacritic_game('BioShock',cur)
         
+        #Something here for sorting out genre labels
+
+        game_data, words_list = get_matching_indie_games('Sci-Fi', '%Shooter', 'single_player')
+        print game_data[-1]
+
+        words_list = np.array(words_list)[:,0]
+        print words_list[0:100]
+
+        #Separating out the summary for this game
+        my_summary = my_game[-1]
         
