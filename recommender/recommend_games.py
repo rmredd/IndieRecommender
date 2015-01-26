f0db403ad79263aa8286da8a599bcde4e1b16942
Rachel Reddick <rr4idic@gmail.com>
2015-01-26 13:41:44 -0800

    Overall run function is fully updated; theoretically, this should work now

--- recommender/recommend_games.py
+++ recommender/recommend_games.py
@@ -238,12 +238,10 @@ def get_all_words_distance(words_indie_matrix, words_vector):
 
     return dist
 
-def get_best_match_with_rating():
-    return 
-
-def run_everything_on_input_title(title, cur):
+def run_everything_on_input_title(title, cur, nvalues=5):
     '''
-    Returns title, game_type, theme, indiedb rating
+    Returns title, game_type, theme, indiedb rating, and similarity rating
+    Returns nvalues total matches, default 5
     '''
     my_game = get_metacritic_game(title,cur)
         
@@ -254,26 +252,36 @@ def run_everything_on_input_title(title, cur):
     game_data, words_list = get_matching_indie_games(genres, game_types, players,cur)
     
     words_list = np.array(words_list)[:,0]
-
+    #Make the dict we need to get the indices right
+    words_index = {}
+    for i in range(len(words_list)):
+        words_index[words_list[5:]] = i
+    
     #Separating out the summary for this game
     my_summary = my_game[-1]
+    
+    #Read in the idf normalizations
+    cur.execute("SELECT * FROM idf_vals WHERE Id = 1")
+    idf = cur.fetchone()
+    idf = idf[1:]
+    idf = np.array(idf).astype(float)
+    leftovers = cur.fetchall() #In case something weird happens and there's more than one row
 
-    words_vector = make_metacritic_game_words_vector(my_summary, words_list)
+    words_vector = make_metacritic_game_words_vector(my_summary, words_index, idf)
         
     #Find the "distance" to each of the other games
-    words_indie_matrix = np.array(game_data)[:,-len(words_vector):].astype(int)
-    words_indie_matrix = (words_indie_matrix > 0).astype(int)
-            
-    dist = get_all_words_distance(words_indie_matrix, words_vector)
+    words_indie_matrix = np.array(game_data)[:,-len(words_vector):].astype(float)
+
+    similarity_rating = get_all_words_distance(words_indie_matrix, words_vector)
     rating = np.array(game_data)[:,1].astype(float)
     votes = np.array(game_data)[:,2].astype(int)
     rating_subset = np.where( (rating > 7) & (votes > 20))[0]
 
-    sorted = rating_subset[np.argsort(dist[rating_subset])]
+    sorted = rating_subset[np.argsort(similarity_rating[rating_subset])]
 
     game_data_arr = np.array(game_data)
 
-    return game_data_arr[sorted[:5], 0], game_data_arr[sorted[:5],4], game_data_arr[sorted[:5],3], rating[sorted[:5]]
+    return game_data_arr[sorted[:nvalues], 0], game_data_arr[sorted[:nvalues],4], game_data_arr[sorted[:nvalues],3], rating[sorted[:nvalues]], similarity_rating[sorted[:nvalues]]
 
 if __name__ == '__main__':
     con = login_mysql("../login.txt")
@@ -281,7 +289,7 @@ if __name__ == '__main__':
     with con:
         cur = con.cursor()
         
-        titles, game_types, themes, ratings = run_everything_on_input_title('BioShock', cur)
+        titles, game_types, themes, ratings, sim_ratings = run_everything_on_input_title('BioShock', cur)
         for i in range(len(titles)):
 
-            print titles[i], game_types[i], themes[i], ratings[i]
+            print titles[i], game_types[i], themes[i], ratings[i], sim_ratings[i]
