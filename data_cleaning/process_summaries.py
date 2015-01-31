42ff1482944e0bf6127def4dc1fb023add0492ce
Rachel Reddick <rr4idic@gmail.com>
2015-01-31 11:41:56 -0800

    Setting up creation of pickle file to contain table of words data

--- data_cleaning/process_summaries.py
+++ data_cleaning/process_summaries.py
@@ -5,6 +5,7 @@ import re
 import nltk
 import MySQLdb as mdb
 import pandas as pd
+import cPickle as pickle
 
 import title_cleanup
 from login_script import login_mysql
@@ -12,27 +13,27 @@ from login_script import login_mysql
 #Scripts for cleaning up the summary data and creating a useful array for the text
 
 def clean_summary_text(text):
-    '''                                                                                                                                                                                           
+    '''
     Main set of routines that cleans up the text, ready for stemming
     Note that this also removes HTML code
     '''
 
-    #Make everything lowercase                                                                                                                                                                    
+    #Make everything lowercase
     clean_text = text.lower()
 
-    #Strip HTML tags, repleace with a space                                                                                                                                                       
+    #Strip HTML tags, repleace with a space
     clean_text = re.sub(r'<[^<>]+>',' ',clean_text)
 
-    #Replace numbers with 'number'                                                                                                                                                                
+    #Replace numbers with 'number'
     clean_text = re.sub(r'[0-9]+', r'number',clean_text)
 
-    #Handle URLs                                                                                                                                                                                  
+    #Handle URLs
     clean_text = re.sub(r'(http|https)://[^\s]*', r'httpaddr',clean_text)
 
-    #Handle email addresses -- strings containing '@'                                                                                                                                             
+    #Handle email addresses -- strings containing '@'
     clean_text = re.sub(r'[^\s]+@[^\s]+', r'emailaddr',clean_text)
 
-    #Replace dollar sign                                                                                                                                                                          
+    #Replace dollar sign
     clean_text = re.sub(r'[$]+', r'dollar',clean_text)
 
     #Remove any remaining alphanumeric characters
@@ -245,6 +246,75 @@ def produce_database_of_common_words(words_list, game_ids, cur, nwords=1000):
     return
 
 
+def produce_pickle_of_common_words(words_list, game_ids, cur, output_dir, nwords=1000):
+    '''
+    Works the same as produce_database_of..., but saves the data to a pair of pickle files
+    instead of to the MySQL database, because the number of columns in the array of word values
+    is generally too large
+    Note that this still creates the idf_vals table in the database
+    '''
+
+    #Get the number of documents (=number of indie games)
+    ndocs = len(words_list)
+    
+    print "Getting overall word list.  This may take a little while..."
+    all_words = get_all_words_list(words_list)
+    words_series = pd.Series(all_words)
+
+    print "Moving on..."
+    #Get the 1000 most common words
+    words_common = words_series.value_counts()
+    words_common_text = words_common.index[:nwords]
+    words_common_count = words_common[:nwords]
+    print "Total number of words (non-unique): ",len(all_words)
+    print "Common words: ",words_common_text
+    #Create a dictionary object for sorting out words later
+    words_common_index = dict([])
+    for i in range(nwords):
+        words_common_index[words_common_text[i]] = i
+
+    #Make the idf weights
+    words_ndocs = get_num_docs_with_words(words_common_index, words_list)
+    idf = get_idf(ndocs, words_ndocs)
+
+    #Make the empty array for holding the tf_idf results
+    words_tf_idf = np.zeros([len(words_list), nwords])
+    #Make the data matrix
+    for i in range(len(words_list)):
+        if i % 1000 == 0:
+            print "Finished through ", i
+            
+        words_tf_idf[i] = get_tf_idf(words_common_index,words_list[i], idf)
+    
+    #Pickle this array
+    pickle.dump(words_tf_idf, open(output_dir+"words_tf_idf.pkl", 'wb') )
+
+    #Create a shorter table for saving the baseline idf values
+    cur.execute("DROP TABLE IF EXISTS idf_vals")
+    create_command = "CREATE TABLE idf_vals(Id INT PRIMARY KEY AUTO_INCREMENT"
+    for i in range(nwords):
+        create_command += ", stem_" + words_common_text[i] + " FLOAT"
+    create_command += ")"
+    print create_command
+    cur.execute(create_command)
+    insert_command = "INSERT INTO idf_vals("
+    for i in range(nwords):
+        if i == 0:
+            insert_command += "stem_" + words_common_text[0]
+        else:
+            insert_command += ", stem_" + words_common_text[i]
+    insert_command += ") VALUES ("
+    for i in range(nwords):
+        if i == 0:
+            insert_command += str(idf[i])
+        else:
+            insert_command += ", "+str(idf[i])
+    insert_command += ")"
+    cur.execute(insert_command)
+
+    return
+
+
 if __name__ == "__main__":
     #Run the whole darn thing to make the database we need
     con = login_mysql('../login.txt')
@@ -255,6 +325,7 @@ if __name__ == "__main__":
         words_list, game_ids = get_words_from_database(cur)
         print "Data acquired."
 
-        produce_database_of_common_words(words_list, game_ids, cur, nwords=1000)
+        #produce_database_of_common_words(words_list, game_ids, cur, nwords=1000)
+        produce_database_of_common_words(words_list, game_ids, cur, "../", nwords=1000)
 
     print "All done!"
